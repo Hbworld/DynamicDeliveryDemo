eb810a30f4f99a552a44e7a1a7da87f1efd1ce40
hemantbansal <hemant.bansal@paytm.com>
2019-03-21 13:52:21 +0530

    initial commit

--- dynamic_feature/src/main/java/com/test/dynamic_feature/DynamicInitActivity.java
+++ dynamic_feature/src/main/java/com/test/dynamic_feature/DynamicInitActivity.java
@@ -1,9 +1,11 @@
 package com.test.dynamic_feature;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.play.core.splitcompat.SplitCompat;
 import com.test.library1.LibraryActivity;
 
 public class DynamicInitActivity extends AppCompatActivity {
@@ -14,5 +16,11 @@ public class DynamicInitActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dynamic_init);
 
         startActivity(new Intent(this, LibraryActivity.class));
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+        SplitCompat.install(this.getApplicationContext());
     }
 }
